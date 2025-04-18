a74531886ec27886062952a11a1664c045c482dd
legend4tech <ajuludennis14@gmail.com>
2025-04-18 03:42:54 +0100

    fixed erorr

--- components/root/CommentsSection.tsx
+++ components/root/CommentsSection.tsx
@@ -169,7 +169,7 @@ export function CommentsSection() {
                                   } else {
                                     commentForm.setError("file", {
                                       type: "manual",
-                                      message: "Image must be 2MB or less",
+                                      message: "Image must less than 2MB ",
                                     });
                                   }
                                 }
