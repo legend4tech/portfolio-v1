2670447209166b040bff0f2f5344a8bfa9c3d069
legend4tech <ajuludennis14@gmail.com>
2025-10-16 19:15:09 +0100

    feat: added updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -101,6 +102,7 @@ export default function RootLayout({
         <Providers>
           <main>{children}</main>
           <Toaster position="top-right" closeButton richColors />
+          <Analytics/>
         </Providers>
       </body>
     </html>
